2e607858e74a6306e2b05235be4253a2669fe555
Tortes <tortes.sun@gmail.com>
2026-07-25 15:21:43 +0800

    Align tab width with fixed title layout

--- config/appearance.lua
+++ config/appearance.lua
@@ -72,7 +72,7 @@ return {
    enable_tab_bar = true,
    hide_tab_bar_if_only_one_tab = false,
    use_fancy_tab_bar = false,
-   tab_max_width = 32,
+   tab_max_width = 28,
    show_tab_index_in_tab_bar = true,
    switch_to_last_active_tab_when_closing_tab = true,
 
